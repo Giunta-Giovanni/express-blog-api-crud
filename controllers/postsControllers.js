96b764de4135763958feae0b1c230e8d549f44a2
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-06 16:52:59 +0100

    [feat] function template & export

---
+++ controllers/postsControllers.js
@@ -0,0 +1,32 @@
+// ci spostiamo i dati in cima
+const menu = require('../data/postsData');
+
+
+
+//FUNCTION -> inseriamo le funzioni delle operazioni crud e la loro logica dandogli i nomi delle stesse operazione
+// index
+function index(req, res){
+
+};
+// show
+function show(req, res){
+
+};
+// store
+function store(req, res){
+
+};
+// update
+function update(req, res){
+
+};
+// modify
+function modify(req, res){
+
+};
+// destroy
+function destroy(req, res){
+
+};
+
+module.exports = {index, show, store, modify, destroy};
