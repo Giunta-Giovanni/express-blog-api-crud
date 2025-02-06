019d07e07d12b49688d775113fb218b39840bd08
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-06 17:00:38 +0100

    [feat] import controller in router and manage all crud

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -6,27 +6,28 @@ const menu = require('../data/postsData');
 //FUNCTION -> inseriamo le funzioni delle operazioni crud e la loro logica dandogli i nomi delle stesse operazione
 // index
 function index(req, res){
-
+    res.send('la mia lista di posts')
 };
 // show
 function show(req, res){
-
+    res.send(`mostra il post ${req.params.id}`);
 };
 // store
 function store(req, res){
-
+    res.send('crea un nuovo post');
 };
 // update
 function update(req, res){
-
+    res.send(`aggiorna il post ${req.params.id}`);
 };
 // modify
 function modify(req, res){
-
+    res.send(`modifica il post ${req.params.id}`);
 };
 // destroy
 function destroy(req, res){
+    res.send(`elimina il post ${req.params.id}`);
 
 };
 
-module.exports = {index, show, store, modify, destroy};
+module.exports = {index, show, store, update, modify, destroy};
--- router/postsRouter.js
+++ router/postsRouter.js
@@ -2,37 +2,27 @@
 const express = require ('express');
 // creiamo una variabile router il cui valore sarà un istanza di express.Router()
 const router = express.Router();
-//importiamo i dati
-const data = require('../data/postsData')
+//importiamo il controller dei posts
+const postsController = require('../controllers/postsControllers')
 
 // ROTTE CRUD
 // index
-router.get('/', function (req, res){
-    res.json(data)
-})
+router.get('/', postsController.index);
 
 // show
-router.get('/:id', function (req, res){
-    res.send('mostra il post' + req.params.id);
+router.get('/:id', postsController.show);
 
-})
 // store
-router.post('/', function (req, res){
-    res.send('crea un nuovo post');
+router.post('/', postsController.store);
 
-})
 // update
-router.put('/:id', function (req, res){
-    res.send('aggiorna il post' + req.params.id);
-})
+router.put('/:id', postsController.update);
+
 // modify
-router.patch('/:id', function (req, res){
-    res.send('modifica il post' + req.params.id);
-})
+router.patch('/:id', postsController.modify);
+
 // destroy
-router.delete('/:id', function (req, res){
-    res.send('elimina il post' + req.params.id);
-})
+router.delete('/:id', postsController.destroy);
 
 
 
