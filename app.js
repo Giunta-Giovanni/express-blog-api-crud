daf8ca1ddc0f6d6f8d27e5a0fb018cebf4de6489
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-06 16:17:07 +0100

    [feat] import, initialization of express & creation of the first route

---
+++ app.js
@@ -0,0 +1,18 @@
+//importiamo express
+const express = require ('express');
+// inizializziamo express e lo salviamo in variabile
+const app = express();
+// salviamo la porta del serve che vogliamo utilizzare
+const port = 3000;
+
+// colleghiamo la cartella con i file statici
+app.use(express.static('public'));
+
+// definiamo la prima rotta --> HOME
+app.get('/', (req, res) =>{
+    res.send ('benvenuti nella mia home')
+});
+
+app.listen(port, () =>{
+    console.log(`la porta di utilizzo è ${port}`)
+})
