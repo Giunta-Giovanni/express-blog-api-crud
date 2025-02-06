c02be4cdba43a3b7aaef8cb9c9167e4c30f4d6d9
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-06 17:33:40 +0100

    [feat] crud show res object in json format

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -7,22 +7,40 @@ const data = require('../data/postsData');
 function index(req, res){
     res.json(data);
 };
+
 // show
 function show(req, res){
-    res.send(`mostra il post ${req.params.id}`);
+    // res.send(`mostra il post ${req.params.id}`);
+
+    // recuperiamo il parametro dinamico dell'id e convertiamolo in numero salvandolo in variabile
+    const id = parseInt(req.params.id);
+    
+    // utilizziamo il metodo find per identificare e farci restituire l'elemento corrispondente
+    const post = data.find(post => post.id === id)
+    // const post = data.find(post =>{
+    //   if(post.id === id){
+    //     return post }
+    // })
+
+    // restituiamo il dato in formato jspm
+    res.json(post)
 };
+
 // store
 function store(req, res){
     res.send('crea un nuovo post');
 };
+
 // update
 function update(req, res){
     res.send(`aggiorna il post ${req.params.id}`);
 };
+
 // modify
 function modify(req, res){
     res.send(`modifica il post ${req.params.id}`);
 };
+
 // destroy
 function destroy(req, res){
     res.send(`elimina il post ${req.params.id}`);
