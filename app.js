0b8e29e2f1ddde66d952d1ad497167726b901a75
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-20 15:48:31 +0100

    [fix] localhost port correction

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // Abilitiamo Cors
 app.use(cors({
-    origin: 'http://localhost:5174/'
+    origin: 'http://localhost:5173'
 }));
 
 // definiamo la prima rotta --> HOME
